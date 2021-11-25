86c4b3804b3e1a877f8b43f05e51683f0d8ad108
Dmitry <sapega.99di@gmail.com>
2021-11-25 20:10:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Core.TestGenerator.Implementations;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = NUnitTestGenerator.CreateGenerator(builder =>
+            {
+                builder.MaxGenerateFiles = 10;
+                builder.MaxReadFiles = 10;
+                builder.MaxWriteFiles = 10;
+            });
+            generator.GenerateTestsAsync(@"C:\Users\sapeg\OneDrive\Рабочий стол\SPP\SPP4\spp-lab4\ConsoleApp\input",
+                @"C:\Users\sapeg\OneDrive\Рабочий стол\SPP\SPP4\spp-lab4\ConsoleApp\output").Wait();
+        }
+    }
+}
